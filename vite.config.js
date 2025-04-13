27d2e337d12ca1e38e5d822307ecd2baf4c253a1
Slava <aks8slavent@gmail.com>
2025-04-13 19:38:27 +0300

    Card product style and product list edit2

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     allowedHosts: [
-      'becf31969ee36eb56903afa429877bf7.serveo.net'
+      'ef3c668006fbfeb7f1cb35a1c8e7922e.serveo.net'
     ]
   }
 });
